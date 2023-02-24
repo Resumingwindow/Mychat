076ba18746389deddd8695f52cbbc426c4b88e69
Resuming  Window <resuingwindow@gmail.com>
2023-02-24 14:05:14 +0530

    sign in with google developed

--- app/src/main/java/com/shubh/mychat/SignInActivity.java
+++ app/src/main/java/com/shubh/mychat/SignInActivity.java
@@ -3,22 +3,37 @@ package com.shubh.mychat;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
+import com.shubh.mychat.Models.User;
 import com.shubh.mychat.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
 ActivitySignInBinding binding;
 ProgressDialog progressDialog;
 FirebaseAuth auth;
+GoogleSignInClient mGoogleSignInClient;
+FirebaseDatabase database;
 //private static final String TAG = "SignInActivity";
 //int AUTHUI_REQUEST_CODE =1001;
 
@@ -34,11 +49,18 @@ FirebaseAuth auth;
 
 
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         progressDialog = new ProgressDialog(SignInActivity.this);
         progressDialog.setTitle("Loging In");
         progressDialog.setMessage("Loging in to your account");
 //      new code  configure google sign in
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this , gso);
+
 //        if (FirebaseAuth.getInstance().getCurrentUser()!=null){
 //            startActivity(new Intent(this,MainActivity.class));
 //            this.finish();
@@ -57,6 +79,7 @@ FirebaseAuth auth;
                         if (task.isSuccessful()){
                             Intent intent = new Intent(SignInActivity.this ,MainActivity.class);
                             startActivity(intent);
+                            Toast.makeText(SignInActivity.this,"Signed in", Toast.LENGTH_SHORT).show();
                         }
                         else {
                             Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -76,6 +99,7 @@ FirebaseAuth auth;
             }
         });
 
+
         if (auth.getCurrentUser()!=null){
             Intent intent =new Intent(SignInActivity.this, MainActivity.class);
             startActivity(intent);
@@ -83,13 +107,65 @@ FirebaseAuth auth;
         binding.btnSignInGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(SignInActivity.this, "We are working in it", Toast.LENGTH_SHORT).show();
+               signIn();
             }
         });
 
 
 
     }
+    int RC_SIGN_IN =65;
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+       if (requestCode==RC_SIGN_IN){
+           Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+           try {
+               GoogleSignInAccount account = task.getResult(ApiException.class);
+               Log.d("TAG", "FirebaseAuthWthGoogle"+account.getId());
+               firebaseAuthWithGoogle(account.getIdToken());
+           }
+           catch (ApiException  e) {
+               Log.w("TAG", "Google Sign in failed", e);
+           }
+
+    }
+}
+private void firebaseAuthWithGoogle(String idToken){
+    AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+    auth.signInWithCredential(firebaseCredential)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("TAG", "signInWithCredential:success");
+                FirebaseUser user = auth.getCurrentUser();
+                User users = new User();
+                users.setUserId(user.getUid());
+                users.setUserName(user.getDisplayName());
+                users.setProfilepic(user.getPhotoUrl().toString());
+
+                database.getReference().child("User").child(user.getUid()).setValue(users);
+
+                    Intent intent = new Intent(SignInActivity.this ,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(SignInActivity.this,"Signed in with google", Toast.LENGTH_SHORT).show();
+
+//                updateUI(user);
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w("TAG", "signInWithCredential:failure", task.getException());
+//                updateUI(null);
+            }
+        }
+    });
+}
 ////    new code
 //    public void SignInActivity(View view){
 //        List<AuthUI.IdpConfig> provider = Arrays.asList(
